b38d06094dfdc418baf6539e63c19bf7a26aac7d
Garrett Davidson <gdavids@purdue.edu>
2016-01-18 16:55:27 -0500

    Switch to clearer naming conventions

--- XboxJoystickMap.h
+++ XboxJoystickMap.h
@@ -9,36 +9,36 @@
 #define SRC_XBOXJOYSTICKMAP_H_
 
 enum XboxJoystickButton {
-	buttonA = 1,
-	buttonB,
-	buttonX,
-	buttonY,
-	buttonLeftBumper,
-	buttonRightBumper,
-	buttonBack,
-	buttonStart,
-	buttonLeftStick,
-	buttonRightStick
+	XboxButtonA = 1,
+	XbobxButtonB,
+	XboxButtonX,
+	XboxButtonY,
+	XboxButtonLeftBumper,
+	XboxButtonRightBumper,
+	XboxButtonBack,
+	XboxButtonStart,
+	XboxButtonLeftStick,
+	XboxButtonRightStick
 };
 
 enum XboxJoystickAxis {
-	axisLeftStickX = 1,
-	axisLeftStickY,
-	axisLeftTrigger,
-	axisRightTrigger,
-	axisRightStickX,
-	axisRightStickY
+	XboxAxisLeftStickX = 1,
+	XboxAxisLeftStickY,
+	XboxAxisLeftTrigger,
+	XboxAxisRightTrigger,
+	XboxAxisRightStickX,
+	XboxAxisRightStickY
 };
 
 enum XboxDPadDirection {
-	dPadUp = 0,
-	dPadUpLeft = 45,
-	dPadLeft = 90,
-	dPadDownLeft = 135,
-	dPadDown = 180,
-	dPadDownRight = 225,
-	dPadRight = 270,
-	dPadUpRight = 315
+	XboxDPadUp = 0,
+	XboxDPadUpLeft = 45,
+	XboxDPadLeft = 90,
+	XboxDPadDownLeft = 135,
+	XboxDPadDown = 180,
+	XboxDPadDownRight = 225,
+	XboxDPadRight = 270,
+	XboxDPadUpRight = 315
 };
 
 #endif /* SRC_XBOXJOYSTICKMAP_H_ */
